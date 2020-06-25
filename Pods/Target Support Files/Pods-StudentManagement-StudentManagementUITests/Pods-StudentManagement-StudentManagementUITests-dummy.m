ccc8bb0d647ca30265427cafdea94a971cd0a9f2
Sathsara Maduranga <sathsaramaduranga@MacBook-Air.local>
2020-06-25 10:26:06 +0530

    V.1

---
+++ Pods/Target Support Files/Pods-StudentManagement-StudentManagementUITests/Pods-StudentManagement-StudentManagementUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_StudentManagement_StudentManagementUITests : NSObject
+@end
+@implementation PodsDummy_Pods_StudentManagement_StudentManagementUITests
+@end
---
+++ StudentManagement/VC+VM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StudentManagement
+//
+//  Created by Sathsara Maduranga on 6/25/20.
+//  Copyright © 2020 Sathsara Maduranga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
